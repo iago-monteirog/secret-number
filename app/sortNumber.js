34a96605eb7fa701afa0328a8b6e05216735e869
iago-monteirog <iagomgarcia9000@gmail.com>
2023-02-23 11:36:02 -0300

    feat: generate secret number randomly

---
+++ app/sortNumber.js
@@ -0,0 +1,14 @@
+const lowerValue = 1;
+const highestValue = 1000;
+
+const secretNumber = generateRandomNumber();
+
+function generateRandomNumber() {
+    return parseInt(Math.random() * highestValue + 1);
+}
+
+const lowerValueElement = document.getElementById('menor-valor');
+lowerValueElement.innerHTML = lowerValue;
+
+const highestValueElement = document.getElementById('maior-valor');
+highestValueElement.innerHTML = highestValue;
